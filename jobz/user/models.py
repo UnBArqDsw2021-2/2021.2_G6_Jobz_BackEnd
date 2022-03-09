711a3ce7bd37faba3bf4dfd998e70f1a37ccd0c5
AlGouvea <alvarohsgouvea@gmail.com>
2022-03-09 14:59:14 -0300

    Adicionados paramêtros de autorização da classe de usuário

--- jobz/user/models.py
+++ jobz/user/models.py
@@ -1,12 +1,29 @@
 from statistics import mode
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
-class Person(models.Model):
+class Person(AbstractBaseUser):
     name = models.CharField(max_length=500)
     cpf = models.BigIntegerField(primary_key=True)
     phone = models.BigIntegerField()
-    email = models.EmailField(max_length=150)
+    email = models.EmailField(verbose_name="email", max_length=150, unique=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDs = ['name', 'cpf']
+
+    def __str__(self):
+        return self.name
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perm(self, app_label):
+        return True
 
 class User(Person):
     # cpf = models.ForeignKey(Person, on_delete=models.CASCADE)
