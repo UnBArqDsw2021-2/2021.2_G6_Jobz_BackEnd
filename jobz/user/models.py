4342619b1a45944d102874800cb02b33eb07572c
AlGouvea <alvarohsgouvea@gmail.com>
2022-03-10 10:28:20 -0300

    Substituído a classe de user padrão

--- jobz/user/models.py
+++ jobz/user/models.py
@@ -1,29 +1,41 @@
 from statistics import mode
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+# Sobreescrever criação de user
+class PersonManager(BaseUserManager):
+    def _create_user(self, username, name, email, password, cpf, phone, is_staff, is_superuser):
+        if not name:
+            raise ValueError('Usuario deve possuir nome')
+        email = self.normalize_email(email)
+        user = self.model(username=username, name=name, email=email, is_staff=is_staff, is_active=True, is_superuser=is_superuser, cpf=cpf, phone=phone)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, name, cpf, phone, email=None, password=None):
+        return self._create_user(username, name, email, password, cpf, phone, False, False)
+
+    def create_superuser(self,username, name, cpf, phone, email=None, password=None):
+        user=self._create_user(username, name, email, password, cpf, phone, True, True)
+        user.is_active=True
+        user.save(using=self._db)
+        return user
 
 # Create your models here.
-class Person(AbstractBaseUser):
+class Person(AbstractUser):
     name = models.CharField(max_length=500)
     cpf = models.BigIntegerField(primary_key=True)
     phone = models.BigIntegerField()
     email = models.EmailField(verbose_name="email", max_length=150, unique=True)
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDs = ['name', 'cpf']
+    REQUIRED_FIELDS = []
+
+    objects = PersonManager()
 
     def __str__(self):
         return self.name
-
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
-    
-    def has_module_perm(self, app_label):
-        return True
 
 class User(Person):
     # cpf = models.ForeignKey(Person, on_delete=models.CASCADE)
