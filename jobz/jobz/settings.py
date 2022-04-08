35592bc6330436876845a6c779a3771ecdaa03ca
AlGouvea <alvarohsgouvea@gmail.com>
2022-04-08 17:53:42 -0300

    Adicionado o restframework_simplejwt para conectar login ao front
    Co-authored-by: Fernando <ilusdasilva@gmail.com>

--- jobz/jobz/settings.py
+++ jobz/jobz/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'service',
+    'search',
     'user',
 ]
 
@@ -139,5 +141,25 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('JWT',),
+    'USER_ID_FIELD': 'email',
+    'USER_ID_CLAIM': 'user_email',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
 }
--- jobz/jobz/urls.py
+++ jobz/jobz/urls.py
@@ -1,17 +1,18 @@
 from django.urls import include, path
 from rest_framework import routers
-from service import views
 from service.views import ServiceViewSet
 from user.views import UserViewSet, ProviderViewSet
+from search.views import OccupationViewSet
 
 router = routers.DefaultRouter()
 router.register(r'service', ServiceViewSet)
 router.register(r'user', UserViewSet)
 router.register(r'provider', ProviderViewSet)
+router.register(r'occupation', OccupationViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/', include('authentication.urls'))
 ]
