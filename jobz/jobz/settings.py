29af313f6d82ffc5c1e41de8d5f03631ae15dfce
LorrayneCardozo <lorraynecardozo15@gmail.com>
2022-04-17 10:07:35 -0300

    Correção
    Co-authored-by: antonioaldisio <aldisiofilho@gmail.com>

--- jobz/jobz/settings.py
+++ jobz/jobz/settings.py
@@ -168,7 +168,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=200),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": False,
